0ad5d5047a6d3b152b9e151d21796d344138d289
Manuela Quiroz <manuquirozy@gmail.com>
2021-09-15 13:52:04 -0500

    setup cypress configuration

---
+++ cypresss-workshop/cypress/integration/google.test.ts
@@ -0,0 +1,6 @@
+     describe('This is the first example of cypress', () => {
+         it('should have a title', () => {
+             cy.visit('https://www.google.com/');
+             cy.title().should('be.equal', 'Google');
+         });
+     });
