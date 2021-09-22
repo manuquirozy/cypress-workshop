64424e01d7845ee075f1183fa9b1e04681d9fd23
Manuela Quiroz Yepes <40613947+manuquirozy@users.noreply.github.com>
2021-09-22 13:54:31 -0500

    add static code analysis

--- cypress/integration/google.test.ts
+++ cypress/integration/google.test.ts
@@ -1,6 +1,6 @@
-     describe('This is the first example of cypress', () => {
-         it('should have a title', () => {
-             cy.visit('https://www.google.com/');
-             cy.title().should('be.equal', 'Google');
-         });
-     });
+describe('This is the first example of cypress', () => {
+  it('should have a title', () => {
+    cy.visit('https://www.google.com/');
+    cy.title().should('be.equal', 'Google');
+  });
+});
