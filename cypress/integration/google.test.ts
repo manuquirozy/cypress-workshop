622618593bd473d62bdc199f8b115114dc4d0e90
Manuela Quiroz Yepes <40613947+manuquirozy@users.noreply.github.com>
2021-09-24 11:10:06 -0500

    debugging the code

--- cypress/integration/google.test.ts
+++ cypress/integration/google.test.ts
@@ -1,6 +1,7 @@
 describe('This is the first example of cypress', () => {
   it('should have a title', () => {
     cy.visit('https://www.google.com/');
+    debugger;
     cy.title().should('be.equal', 'Google');
   });
 });
